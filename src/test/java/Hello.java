0af40ce5b013015295d703c2986dcb51d627e0a2
ZeynepOzkan <72092080+ZeynepOzkan@users.noreply.github.com>
2020-10-13 19:29:21 +0200

    Update Hello.java
    updated in remote repository

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Really Awesome hello");
+        System.out.println("Really Awesome hello made some changes awesome hello");
+        
+      
+  
 
 
 
